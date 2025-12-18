76853eb703597cc91b59b577bce07ad87cb54b2e
Saurabh Sardeshpande <saurabh.ss@gmail.com>
2025-12-18 00:07:11 -0800

    Unique ID challenge complete

---
+++ cmd/maelstrom-unique-ids/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/google/uuid"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	ts := 1
+
+	// Register a handler for the "echo" message that responds with an "echo_ok".
+	n.Handle("echo", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type.
+		body["type"] = "echo_ok"
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		ts++
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "generate_ok"
+		body["id"] = uuid.New().String()
+
+		return n.Reply(msg, body)
+	})
+
+	// Execute the node's message loop. This will run until STDIN is closed.
+	if err := n.Run(); err != nil {
+		log.Printf("ERROR: %s", err)
+		os.Exit(1)
+	}
+}
+
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/saurabh/fly
+
+go 1.21
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/jepsen-io/maelstrom/demo/go v0.0.0-20251128144731-cb7f07239012
+)
