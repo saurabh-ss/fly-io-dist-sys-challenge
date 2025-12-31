2fb2b5350c8b3badc35d39d0bbcc6f1c82639d8d
Saurabh Sardeshpande <saurabh.ss@gmail.com>
2025-12-31 03:05:03 -0800

    Challenge #6a: Single-Node, Totally-Available Transactions passes

--- cmd/maelstrom-txn/main.go
+++ cmd/maelstrom-txn/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
@@ -23,16 +25,62 @@ type txnResponse struct {
 func main() {
 	n := maelstrom.NewNode()
 
+	// Linearizable key-value store
+	// A sequential store seems to work too
+	kv := maelstrom.NewLinKV(n)
+
 	n.Handle("txn", func(msg maelstrom.Message) error {
-		var body txnRequest
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
+		var req txnRequest
+		if err := json.Unmarshal(msg.Body, &req); err != nil {
 			return err
 		}
 
 		resp := txnResponse{
 			Type:      "txn_ok",
-			InReplyTo: body.MsgID,
-			Txn:       body.Txn,
+			InReplyTo: req.MsgID,
+			Txn:       make([][]any, 0, len(req.Txn)),
+		}
+
+		ctx := context.Background()
+
+		// Store read results to maintain original order in the response
+		readResults := make([]any, len(req.Txn))
+
+		// Pass 1: Perform all reads
+		for i, op := range req.Txn {
+			kind := op[0].(string)
+			if kind == "r" {
+				key := fmt.Sprintf("%v", op[1])
+				val, err := kv.ReadInt(ctx, key)
+				if err != nil {
+					readResults[i] = nil
+				} else {
+					readResults[i] = val
+				}
+			}
+		}
+
+		// Pass 2: Perform all writes
+		for _, op := range req.Txn {
+			kind := op[0].(string)
+			if kind == "w" {
+				key := fmt.Sprintf("%v", op[1])
+				err := kv.Write(ctx, key, op[2])
+				if err != nil {
+					log.Printf("write error on key %s: %v", key, err)
+				}
+			}
+		}
+
+		// Reconstruct response in the original order
+		for i, op := range req.Txn {
+			kind := op[0].(string)
+			switch kind {
+			case "r":
+				resp.Txn = append(resp.Txn, []any{kind, op[1], readResults[i]})
+			case "w":
+				resp.Txn = append(resp.Txn, []any{kind, op[1], op[2]})
+			}
 		}
 
 		return n.Reply(msg, resp)
